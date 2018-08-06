1f9e5f9930d684a5df122da56aad49af7c75a0a6
LEE Dongjun <redongjun@gmail.com>
2018-08-06 15:07:14 +0900

    Add license to streamer files

--- source/streamer/streaming-object.cpp
+++ source/streamer/streaming-object.cpp
@@ -1,6 +1,14 @@
 /*
- * Copyright information
- */
+  GiGAeyes Streamer version 1.0
+
+  Copyright ⓒ 2015 kt corp. All rights reserved.
+
+  This is a proprietary software of kt corp, and you may not use this file except in
+  compliance with license agreement with kt corp. Any redistribution or use of this
+  software, with or without modification shall be strictly prohibited without prior written
+  approval of kt corp, and the copyright notice above does not evidence any actual or
+  intended publication of such software.
+*/
 
 #include "streaming-object.h"
 
